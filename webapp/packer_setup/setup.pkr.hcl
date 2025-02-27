c5fde800848f12b53ca048dc8109649ecadd0e1b
Jahnavi Bhagirath Patel <patel.jahnavi@northeastern.edu>
2025-02-27 13:03:06 -0500

    trial gcp

--- webapp/packer_setup/setup.pkr.hcl
+++ webapp/packer_setup/setup.pkr.hcl
@@ -25,6 +25,26 @@ variable "google_project_id" {
   type = string
 }
 
+variable "db_host" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
 
 source "amazon-ebs" "ubuntu" {
   ami_name        = "webapp-ami"
@@ -59,7 +79,7 @@ source "amazon-ebs" "ubuntu" {
 
 source "googlecompute" "ubuntu" {
   project_id          = var.google_project_id
-  source_image_family = "ubuntu-2004-lts"
+  source_image_family = "ubuntu-2404-lts-amd64"
   image_description   = "Webapp GCE Image packer"
   ssh_username        = "ubuntu"
   zone                = "us-east1-b"
@@ -68,17 +88,51 @@ source "googlecompute" "ubuntu" {
 
 build {
   sources = [
-    #     "source.amazon-ebs.ubuntu",
-    "source.googlecompute.ubuntu"
+    "source.amazon-ebs.ubuntu",
+    #     "source.googlecompute.ubuntu"
   ]
   provisioner "shell" {
+    script = "../scripts/installation.sh" # This script installs necessary dependencies and configurations
+  }
+
+  #   set up mysql database
+  # Create the database and user if they do not exist.
+  provisioner "shell" {
+    environment_vars = [
+      "DB_USER=${var.db_user}",
+      "DB_PASSWORD=${var.db_password}",
+      "DB_NAME=${var.db_name}",
+      "DB_HOST=${var.db_host}",
+    ]
+    script = "../scripts/sql_setup.sh"
+  }
+
+
+  #  paste webapp.zip created by github action
+  provisioner "file" {
+    source      = "../../webapp.zip"
+    destination = "/tmp/webapp.zip"
+  }
+
+
+  # Unzip the webapp.zip file to /opt/csye6225
+  provisioner "shell" {
     inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y nginx",
-      "sudo systemctl start nginx",
-      "sudo systemctl enable nginx"
+      "cd /tmp",
+      "sudo unzip -o webapp.zip -d /opt/csye6225"
     ]
   }
 
 
+  #   set up webapp
+  provisioner "shell" {
+    environment_vars = [
+      "DB_USER=${var.db_user}",
+      "DB_PASSWORD=${var.db_password}",
+      "DB_NAME=${var.db_name}",
+      "DB_HOST=${var.db_host}",
+      "SECRET_KEY=${var.secret_key}"
+    ]
+    script = "../scripts/webapp_setup.sh"
+  }
 }
---
+++ webapp/scripts/webapp_setup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# create .env file
+echo "Creating .env file"
+sudo bash -c "cat <<EOF > /etc/.env
+DB_NAME='${DB_NAME}'
+DB_USER='${DB_USER}'
+DB_PASSWORD='${DB_PASSWORD}'
+DB_HOST='${DB_HOST}'
+DB_PORT='${DB_PORT}'
+SECRET_KEY='${SECRET_KEY}'
+VM_IP='*'
+EOF"
+
+
+#Create a new Linux group for the application if it does not exist.
+echo "Creating a new Linux group for the application if it does not exist."
+sudo groupadd -f csye6225
+
+# Create a new Linux user for the application if it does not exist.
+echo "Creating a new Linux user for the application if it does not exist."
+sudo useradd -m -g csye6225 -s /usr/sbin/nologin csye6225
+
+# Create directory for the web application in /opt/csye6225 directory.
+echo "Creating directory for the web application."
+sudo mkdir -p /opt/csye6225
+sudo unzip /tmp/webapp.zip -d /opt/csye6225
+
+#put .env file in /opt/csye6225/webapp
+echo "Moving .env file to /opt/csye6225/webapp"
+sudo cp /etc/.env /opt/csye6225/webapp
+
+# Change the ownership of the directory to the new user and group.
+echo "Changing the ownership of the directory to the new user and group."
+sudo chown -R csye6225:csye6225 /opt/csye6225
+
+# Update the permissions of the folder and artifacts in the directory.
+echo "Updating the permissions of the folder and artifacts in the directory."
+sudo chmod -R 755 /opt/csye6225
+
+#make .venv file
+echo "Creating .venv file"
+sudo mkdir -p /opt/csye6225/webapp/.venv
+#own the directory to csye6225
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/.venv
+#update the permissions of the folder and artifacts in the directory.
+sudo chmod -R 755 /opt/csye6225/webapp/.venv
+
+# Go to the directory where the web application is installed.
+echo "Going to the directory where the web application is installed."
+cd /opt/csye6225/webapp || exit
+
+# Create a virtual environment for the application.
+echo "Creating a virtual environment for the application."
+sudo python3 -m venv /opt/csye6225/venv
+
+# Activate the virtual environment.
+echo "Activating the virtual environment."
+# Install the required packages.
+echo "Installing the required packages."
+sudo bash -c "source /opt/csye6225/venv/bin/activate && pip install -r requirements.txt"
+
+#Migrate the database
+echo "Migrating the database."
+sudo bash -c "source /opt/csye6225/venv/bin/activate && python manage.py makemigrations && python manage.py migrate"
+
+#move the service file to /etc/systemd/system
+echo "Moving the service file to /etc/systemd/system"
+sudo cp /opt/csye6225/webapp/packer_setup/webapp.service /etc/systemd/system/webapp.service
+sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
+
+#start the server
+echo "Starting the server."
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp.service
+
+
+
+
